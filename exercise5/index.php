490e4e0db1d2fb3d082d756cc6ae10e88a6f8d8b
Shierene Cervantes <shierenecervantes23@gmail.com>
2016-11-05 17:46:37 +0800

    Modified something in index.php

--- exercise5/index.php
+++ exercise5/index.php
@@ -8,7 +8,6 @@ $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
  mysqli_query($con,$sql_query);
  header("Location: $_SERVER[PHP_SELF]");
 }
-// del condition
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
