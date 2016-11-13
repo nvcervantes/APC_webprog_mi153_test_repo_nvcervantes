eed832fd31793115de6f5a961b78b277be2e009a
Shierene Cervantes <shierenecervantes23@gmail.com>
2016-11-13 23:09:47 +0800

    Copied exercise5 for reference

---
+++ exercise6/exercise5/add_data.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+include_once 'dbconfig.php';
+if(isset($_POST['btn-save']))
+{
+ // var for input data
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $address = $_POST['address'];
+ $website = $_POST['website'];
+ $comment = $_POST['comment'];
+ $cpnum = $_POST['cpnum'];
+ $gender = $_POST['gender'];
+ $nickname = $_POST['nickname'];
+  
+ // sql query for inserting data into database
+ 
+        $sql_query = "INSERT INTO users(name,email,address, website, comment, cpnum, gender, nickname) VALUES('$name','$email','$address',$website, $comment, $cpnum, $gender, $nickname)";
+ mysqli_query($con,$sql_query);
+        
+ 
+}
+?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="header">
+ <div id="content">
+    <label>CRUD Operations With PHP and MySql - By Cleartuts</label>
+    </div>
+</div>
+<div id="body">
+ <div id="content">
+    <form method="post">
+    <table align="center">
+    <tr>
+    <td align="center"><a href="index.php">back to main page</a></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="name" placeholder="Name" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="email" placeholder="Email" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="address" placeholder="Address" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="website" placeholder="Website" required /></td>
+    </tr>
+    <tr>
+	<tr>
+    <td><input type="text" name="comment" placeholder="Comment" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="cpnum" placeholder="Cellphone Number" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="gender" placeholder="Gender" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="nickname" placeholder="Nickname" required /></td>
+    </tr>
+    <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
+    </tr>
+    </table>
+    </form>
+    </div>
+</div>
+
+</center>
+</body>
---
+++ exercise6/exercise5/edit_data.php
@@ -0,0 +1,108 @@
+<?php
+include_once 'dbconfig.php';
+if(isset($_GET['edit_id']))
+{
+ $sql_query="SELECT * FROM users WHERE user_id=".$_GET['edit_id'];
+ $result_set=mysqli_query($con,$sql_query);
+ $fetched_row=mysqli_fetch_array($con,$result_set);
+}
+if(isset($_POST['btn-update']))
+{
+ // var for input data
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $address = $_POST['address'];
+ $website = $_POST['website'];
+ $comment = $_POST['comment'];
+ $cpnum = $_POST['cpnum'];
+ $gender = $_POST['gender'];
+ $nickname = $_POST['nickname'];
+
+
+ // sql query for update data into database
+ $sql_query = "UPDATE users SET name='$name',email='$email',address='$address',website='$website', comment='$comment',cpnum='$cpnum', gender='$gender', nickname='$nickname' WHERE user_id=".$_GET['edit_id'];
+
+ 
+ // sql query execution function
+ if(mysqli_query($con,$sql_query))
+ {
+  ?>
+  <script type="text/javascript">
+  alert('Data Are Updated Successfully');
+  window.location.href='index.php';
+  </script>
+  <?php
+ }
+ else
+ {
+  ?>
+  <script type="text/javascript">
+  alert('error occured while updating data');
+  </script>
+  <?php
+ }
+ // sql query execution function
+}
+if(isset($_POST['btn-cancel']))
+{
+ header("Location: index.php");
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="header">
+ <div id="content">
+    <label>CRUD Operations With PHP and MySql - By Cleartuts</label>
+    </div>
+</div>
+
+<div id="body">
+ <div id="content">
+    <form method="post">
+    <table align="center">
+    <tr>
+    <td><input type="text" name="name" placeholder="Name" value="<?php echo $fetched_row['name']; ?>" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="email" placeholder="Email" value="<?php echo $fetched_row['email']; ?>" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="address" placeholder="Address" value="<?php echo $fetched_row['address']; ?>" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="website" placeholder="Website" value="<?php echo $fetched_row['website']; ?>" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="comment" placeholder="Comment" value="<?php echo $fetched_row['comment']; ?>" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="cpnum" placeholder="Cellphone Number" value="<?php echo $fetched_row['cpnum']; ?>" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="gender" placeholder="Gender" value="<?php echo $fetched_row['gender']; ?>" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="nickname" placeholder="Nickname" value="<?php echo $fetched_row['nickname']; ?>" required /></td>
+    </tr>
+    <tr>
+    <td>
+    <button type="submit" name="btn-update"><strong>UPDATE</strong></button>
+    <button type="submit" name="btn-cancel"><strong>Cancel</strong></button>
+    </td>
+    </tr>
+    </table>
+    </form>
+    </div>
+</div>
+
+</center>
+</body>
+</html>
