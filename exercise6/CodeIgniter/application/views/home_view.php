79fb1c68ede0b94f0cad173088363ad358390164
Shierene Cervantes <shierenecervantes23@gmail.com>
2016-11-14 00:58:33 +0800

    Edited home_view.php

--- exercise6/CodeIgniter/application/views/home_view.php
+++ exercise6/CodeIgniter/application/views/home_view.php
@@ -7,7 +7,7 @@
 
 <style type="text/css">
 
-   html { 
+html { 
   background: url(lastpic.jpg) no-repeat center center fixed; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
@@ -109,14 +109,34 @@ div.form {
 
 <article>
 <div class="container2"> 
+
   <h1>Shierene &#9752</h1>
   <p>A bookworm whose favorite nocturnal hobby is reading.</p>
-  <p> I am Shierene Cervantes, called Shie by my close friends, whom you cannot really know well without spending some time to get along with. Whenever I ask my friends what their first impressions to me were, they say either I do not look someone you could easily approach, an extremely girly person who only make friends to others in the same level as I am, a person who is so conscious about her appearance, and someone who gets easily annoyed. When people start to know who I am, most of them say that I am a sweet and very reserved girl who is lady-like in every way. </p>
-  <p>However, there is more to me than just that. Unbelievable to some, my close friends know that once you get to really know me, your first impression would definitely change into a new perspective that is quite the opposite as your first. According to them, I am really a girly girl who has something you never thought I have to show off once I get really close to a person, and they are probably referring to my childlike attitude whenever I am with people I am comfortable with. They also say that I can easily get along with different kinds of people for I do not really choose the people I want to be friends with.
+  <p> I am Shierene Cervantes, called Shie by my close friends, whom you cannot really know well without 
+  <br> spending some time to get along with. Whenever I ask my friends what their first impressions to me 
+  <br> were, they say either I do not look someone you could easily approach, an extremely girly person who
+  <br> only make friends to others in the same level as I am, a person who is so conscious about her
+  <br> appearance, and someone who gets easily annoyed. When people start to know who I am, most of them
+  <br> say that I am a sweet and very reserved girl who is lady-like in every way. </p>
+  <p> However, there is more to me than just that. Unbelievable to some, my close friends know that once 
+  <br> you get to really know me, your first impression would definitely change into a new perspective 
+  <br> that is quite the opposite as your first. According to them, I am really a girly girl who has 
+  <br> something you never thought I have to show off once I get really close to a person, and they are 
+  <br> probably referring to my childlike attitude whenever I am with people I am comfortable with.
+  <br> They also say that I can easily get along with different kinds of people for I do not really choose
+  <br> the people I want to be friends with.
 </p>
-<p>Reading books, playing the piano, listening to really good music, seeing little children happy, shopping after a very stressful midterm or final week at school, getting high grades and making my parents proud are some of the things that can really make me happy. I have read a lot of books that inspired me in a lot of different ways and I find reading books really an interesting way to take you places you have never been.
+<p> Reading books, playing the piano, listening to really good music, seeing little children happy,
+ <br> shopping after a very stressful midterm or final week at school, getting high grades and making 
+ <br> my parents proud are some of the things that can really make me happy. I have read a lot of books
+<br> that inspired me in a lot of different ways and I find reading books really an interesting way to 
+<br> take you places you have never been.
 </p>
-<p>I am rarely impressed but eternally fascinated by everything. I know that it seems a bit absurd, but most people find me quite difficult to impress, and perhaps that is because my family always reminds to always strive to be the best even when I think I am already good enough. Nevertheless, I believe that everything God created is wonderful and worth appreciating.</p>
+<p> I am rarely impressed but eternally fascinated by everything. 
+<br> I know that it seems a bit absurd, but most people find me quite difficult to impress, 
+<br> and perhaps that is because my family always reminds to always strive to be the best 
+<br> even when I think I am already good enough. Nevertheless, I believe that everything 
+<br> God created is wonderful and worth appreciating.</p>
   <p>Hello, you!</p>
   <hr>
 </div>
@@ -128,12 +148,16 @@ div.form {
 <p id="hobbyr">What is my favorite hobby?</p>
 	<button type="button" onclick="document.getElementById('hobbyr').innerHTML = 'Reading has always been and will forever be my favorite hobby.'">Fave Hobby</button>
 <p id="gp">What is my guilty pleasure?</p>
+
 	<button type="button" onclick="document.getElementById('gp').innerHTML = 'Definitely Chocolates.'">Guilty Pleasure...</button>
 <p id="pup">What is the name of my first puppy?</p>
+
 	<button type="button" onclick="document.getElementById('pup').innerHTML = 'Toby. &#9829'">First Pup!</button>
 <p id="cd">What is my childhood dream?</p>
+
 	<button type="button" onclick="document.getElementById('cd').innerHTML = 'To be a chemist.'">Childhood Dream</button>
 <p id="plan">What do I want to be in the future?</p>
+
 	<button type="button" onclick="document.getElementById('plan').innerHTML = 'To be an accomplished person doing what I love and not settling for less.'">Ultimate Goal &#9829</button>
 	<p></p>
 	<hr>
