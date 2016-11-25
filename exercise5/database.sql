81f2e865f464cec26c6ad28451374f49341a0749
Shierene Cervantes <shierenecervantes23@gmail.com>
2016-11-25 11:44:33 +0800

    Created database.sql

--- exercise5/database.sql
+++ exercise5/database.sql
@@ -5,7 +5,7 @@ CREATE TABLE `dbtuts`.`users` (
 `email` VARCHAR(50) NOT NULL,
 `address` VARCHAR( 100 ),
 `website` VARCHAR (100),
-`message` VARCHAR(255),
+`comment` VARCHAR(255),
 `cpnum` VARCHAR(14) NOT NULL,
 `gender` VARCHAR(20) NOT NULL,
 `nickname` VARCHAR( 25 ) NOT NULL
