e2c1ba2e733ab4125be018d9a61a892171445118
Shierene Cervantes <shierenecervantes23@gmail.com>
2016-12-09 19:34:21 +0800

    Modified database.php

--- exercise6/database.sql
+++ exercise6/database.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE `dbexercise6` ;
-CREATE TABLE `dbexercise6`.`users` (
-`user_id` INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`name` VARCHAR( 45 ) NOT NULL ,
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
 `nickname` VARCHAR( 25 ) NOT NULL ,
 `email` VARCHAR( 45 ) NOT NULL,
 `address` VARCHAR( 45 ) ,
 `gender` VARCHAR( 25 ) NOT NULL,
 `cpnum` VARCHAR( 45 ) NOT NULL,
-`comment` VARCHAR( 45 ) 
-) ENGINE = InnoDB;
+`comment` VARCHAR( 45 )
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+ 
