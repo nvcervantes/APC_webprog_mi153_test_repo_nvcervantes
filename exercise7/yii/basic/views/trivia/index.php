d3ce74ff59adca0475418d488f505541b620ca1c
Shierene Cervantes <shierenecervantes23@gmail.com>
2016-12-27 19:57:39 +0800

    Edited index.php

--- exercise7/yii/basic/views/trivia/index.php
+++ exercise7/yii/basic/views/trivia/index.php
@@ -2,8 +2,10 @@
 use yii\helpers\Html;
 use yii\widgets\LinkPager;
 ?>
+
 <h1>Trivia</h1>
 <ul>
+
 <?php foreach ($trivia as $trivias): ?>
     <li>
         <?= Html::encode("{$trivias->trivia} ({$trivias->answer})") ?>:
